1ccfd28c1975979c6a781cd1856ad1f76ad6fbf3
Maria Berestovaya <57846761+antaresofficial@users.noreply.github.com>
2022-06-17 22:37:07 +0500

    test

--- main.cpp
+++ main.cpp
@@ -1,18 +1,14 @@
+#include <vector>
 #include <iostream>
-#include <windows.h>
+#include <numeric>  // iota
+#include <algorithm>  // for_each
+
 using namespace std;
 
-int main()
-{
-    SetConsoleCP(65001);
-    SetConsoleOutputCP(65001);
-    
-    int a, b;
-    cout << "Введите первое число: ";
-    cin >> a;
-    cout << "Введите второе число: ";
-    cin >> b;
-    int c = a + b;
-    cout << "Сумма чисел = " << c << endl;
+int main() {
+    vector<int> v(100);
+    iota(v.begin(), v.end(), 1);  // v = [1, 2, 3, ..., 100]
+    for_each(v.begin(), v.end(), [](int& a){a = a*a;});  // v = [1, 4, 9, ..., 10000]
+    for_each(v.begin(), v.end(), [](int a){cout << a << ' ';});
     return 0;
 }
